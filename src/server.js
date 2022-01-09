f08d8fa39994ddde373c9f0eea7e689258cc5b98
Jose1i1o <guadix88@gmail.com>
2022-01-09 21:13:58 +0100

    feat(basic functinalities from scracth): building up babel, nodemon, express, parser and queries by hand

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+    },
+    'learn-node': {
+        upvotes: 0,
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+    },
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} has ${articlesInfo[articleName].upvotes} votes!`);
+})
+
+app.post('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
+    });
+
+
+app.listen(8000, () => console.log('Listening on port 8000'));
